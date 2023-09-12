195b6799fb9c84227ec8591e343cd85c6dc1ccdd
kynail <57899519+kynail@users.noreply.github.com>
2023-09-12 08:36:19 +0200

    petite correction

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Meteo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Meteo App Home Page'),
     );
   }
 }
